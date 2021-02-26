d8b01b7eaf8a4c2b041137c114472fefd5735d24
Josué <josue.santos@svm.com.br>
2021-02-26 08:16:48 -0300

    aula 04 correção testes

--- src/__tests__/Surveys.test.ts
+++ src/__tests__/Surveys.test.ts
@@ -4,35 +4,31 @@ import { app } from '../app';
 import createConnection from '../database';
 
 describe("Surveys", () => {
-    // beforeAll(async () => {
-    //     const connection = await createConnection();
-    //     await connection.runMigrations();
-    // });
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
 
-    // it("Should be able to create a new surveys", async () => {
-    //     const response = await request(app).post("/surveys")
-    //     .send({
-    //         title: "Title Exemple",
-    //         description: "Description Exemple"
-    //     })
-    //     expect(response.status).toBe(201);
-    //     expect(response.body).toHaveProperty("id");
-    // });
+    it("Should be able to create a new surveys", async () => {
+        const response = await request(app).post("/surveys")
+        .send({
+            title: "Title Exemple",
+            description: "Description Exemple"
+        })
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty("id");
+    });
 
-    // it("Should be able to get all surveys", async () => {
-    //     await request(app).post("/surveys")
-    //     .send({
-    //         title: "Title2 Exemple",
-    //         description: "Description2 Exemple"
-    //     });
+    it("Should be able to get all surveys", async () => {
+        await request(app).post("/surveys")
+        .send({
+            title: "Title2 Exemple",
+            description: "Description2 Exemple"
+        });
 
-    //     const response = await request(app).get("/surveys");
+        const response = await request(app).get("/surveys");
 
-    //     expect(response.body.length).toBe(2);
-    //     expect(response.status).toBe(200);
-    // });
-
-    it("Teste Test", async () => {
-        expect(1+1).toBe(2);
+        expect(response.body.length).toBe(2);
+        expect(response.status).toBe(200);
     });
 });
--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -24,25 +24,4 @@ describe("Users", () => {
     });
     expect(response.status).toBe(400);
   });
-
-  it("Should be able to create a new surveys", async () => {
-    const response = await request(app).post("/surveys").send({
-      title: "Title Exemple",
-      description: "Description Exemple",
-    });
-    expect(response.status).toBe(201);
-    expect(response.body).toHaveProperty("id");
-  });
-
-  it("Should be able to get all surveys", async () => {
-    await request(app).post("/surveys").send({
-      title: "Title2 Exemple",
-      description: "Description2 Exemple",
-    });
-
-    const response = await request(app).get("/surveys");
-
-    expect(response.body.length).toBe(2);
-    expect(response.status).toBe(200);
-  });
 });
